a5b6beb2ad8346031436ddeb1f76b62385e90371
Ross Gardiner <github@public.rossng.eu>
2025-01-05 21:38:05 +0100

    Add share links to muural blog post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,10 @@
 
 export default new Map([
-["src/content/posts/2024-09-01-mesh-cutting.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2024-09-01-mesh-cutting.mdx&astroContentModuleFlag=true")],
+["src/content/posts/2017-06-05-loan-shark.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2017-06-05-loan-shark.mdx&astroContentModuleFlag=true")],
+["src/content/posts/2018-08-10-free-trade.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2018-08-10-free-trade.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/2024-09-10-too-many-open-files.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2024-09-10-too-many-open-files.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/2020-04-11-my-first-llvm-program.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2020-04-11-my-first-llvm-program.mdx&astroContentModuleFlag=true")],
-["src/content/posts/2018-08-10-free-trade.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2018-08-10-free-trade.mdx&astroContentModuleFlag=true")],
-["src/content/posts/2017-06-05-loan-shark.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2017-06-05-loan-shark.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/2024-11-27-mit-license-with-disney-clause.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2024-11-27-mit-license-with-disney-clause.mdx&astroContentModuleFlag=true")],
-["src/content/posts/2025-01-05-muural.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2025-01-05-muural.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/2025-01-05-muural.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2025-01-05-muural.mdx&astroContentModuleFlag=true")],
+["src/content/posts/2024-09-01-mesh-cutting.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2024-09-01-mesh-cutting.mdx&astroContentModuleFlag=true")]]);
 		
