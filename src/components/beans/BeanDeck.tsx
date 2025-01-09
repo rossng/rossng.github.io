28e439402b746c50f9c530d34ab7541da41942fb
Ross Gardiner <github@public.rossng.eu>
2025-01-09 19:34:48 +0100

    Finish off bean post

--- src/components/beans/BeanDeck.tsx
+++ src/components/beans/BeanDeck.tsx
@@ -4,8 +4,8 @@ import { beans } from "./BeanData";
 export default function BeanDeck() {
   return (
     <div className="flex flex-wrap items-stretch">
-      {beans.map((bean) => (
-        <div className="w-1/2 h-auto p-2 flex-shrink ">
+      {beans.map((bean, idx) => (
+        <div className="w-1/2 h-auto p-2 flex-shrink" key={idx}>
           <BeanStatsCard bean={bean} className="h-full" />
         </div>
       ))}
