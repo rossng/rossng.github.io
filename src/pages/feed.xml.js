feff75c19909e9699ab248c7e68760018b5cf7cb
Ross Gardiner <github@public.rossng.eu>
2025-01-19 00:55:13 +0100

    Exclude draft posts from feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -14,13 +14,15 @@ export async function GET(context) {
     site: context.site,
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.date,
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
-      link: `/posts/${post.slug}/`,
-    })),
+    items: posts
+      .filter((post) => !post.data.draft)
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.date,
+        // Compute RSS link from post `slug`
+        // This example assumes all posts are rendered as `/blog/[slug]` routes
+        link: `/posts/${post.slug}/`,
+      })),
     // (optional) inject custom xml
     customData: `<language>en-gb</language>`,
   });
